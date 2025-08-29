6d6948dfd30704a4c655bfe907f3fe3c7a58f154
Gustavo Alves <gabs.principal.2005@gmail.com>
2025-08-29 10:02:34 -0300

    commit inicial

---
+++ src/main/java/com/gustavo/neoappcustomerapi/NeoappCustomerApiApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.neoappcustomerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoappCustomerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoappCustomerApiApplication.class, args);
+    }
+
+}
