31af59a7d863eb8ec4034865d242d568faf9df61
Gustavo Alves <gabs.principal.2005@gmail.com>
2025-08-30 22:08:21 -0300

    Método salva usuário concluído

---
+++ src/main/java/com/gustavo/neoappcustomerapi/infrastructure/entity/Cliente.java
@@ -0,0 +1,60 @@
+package com.gustavo.neoappcustomerapi.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "cliente")
+public class Cliente implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+
+    private String cpf; // opcional
+
+    // Getter calculado
+    public Integer getIdade() {
+        return Period.between(this.dataNascimento, LocalDate.now()).getYears();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+}
+
---
+++ src/main/java/com/gustavo/neoappcustomerapi/infrastructure/repository/ClienteRepository.java
@@ -0,0 +1,13 @@
+package com.gustavo.neoappcustomerapi.infrastructure.repository;
+
+import com.gustavo.neoappcustomerapi.infrastructure.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>, JpaSpecificationExecutor<Cliente> {
+    Optional<Cliente> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
+
