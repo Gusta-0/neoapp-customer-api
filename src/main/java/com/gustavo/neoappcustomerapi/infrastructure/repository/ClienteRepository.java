d87a1a880bf0665fd4017ef07da2bd2b115b72df
Gustavo Alves <gabs.principal.2005@gmail.com>
2025-09-02 10:09:00 -0300

    Ajustando regra de negócio Service

--- src/main/java/com/gustavo/neoappcustomerapi/infrastructure/repository/ClienteRepository.java
+++ src/main/java/com/gustavo/neoappcustomerapi/infrastructure/repository/ClienteRepository.java
@@ -1,6 +1,8 @@
 package com.gustavo.neoappcustomerapi.infrastructure.repository;
 
 import com.gustavo.neoappcustomerapi.infrastructure.entity.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -11,5 +13,14 @@ public interface ClienteRepository extends JpaRepository<Cliente, Long>, JpaSpec
     boolean existsByEmail(String email);
 
     void deleteByEmail(String email);
+
+    Optional<Object> findByCpf(String cpf);
+
+    Page<Cliente> findAll(Pageable pageable);
+
+    Page<Cliente> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
+
+    Page<Cliente> findByEmailContainingIgnoreCase(String email, Pageable pageable);
+
 }
 
